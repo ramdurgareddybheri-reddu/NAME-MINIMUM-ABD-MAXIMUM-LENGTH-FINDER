4d04243cef9bee24efdea8a62967faade6b74b69
RAMDURGAREDDY <ramdurgareddybheri@gmail.com>
2026-07-08 16:43:59 +0530

    Add files via upload

---
+++ script25.py
@@ -0,0 +1,8 @@
+name=input("enter your name")
+if len(name)<3:
+    print("THE NAME MINIMUM 3 CHARACTERS REQURIED")
+elif len(name)>50:
+    print("THE NAME MAXIMUM 50 CHARACTERS")
+
+else:
+    print("perfect")
